07b047ce5dffe05eca67f85f572a41e71e798c38
Noorullah <mdnoorullah052@gmail.com>
2022-06-15 11:23:19 +0530

    make it better

---
+++ public/js/client.js
@@ -0,0 +1,119 @@
+const socket = io();
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("msgInput");
+const imageinput = document.getElementById("image");
+const messageContainer = document.querySelector(".wrapper");
+const room_container = document.getElementById("room");
+var audio = new Audio('notify.mp3');
+var userlist = [];
+
+const delete_element = (id) => {
+    var element = document.getElementsByClassName(id)[0];
+    element.remove();
+}
+const append_img = (image, position) => {
+    const messageElement = document.createElement('img');
+    messageElement.src = image;
+    messageElement.classList.add('img');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left' || position=='mid'){
+        audio.play();
+        }
+}
+// append message on chat area
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left' || position=='mid'){
+        audio.play();
+        }
+   
+}
+// update room list
+const append_user = (username) => {
+    const final = document.createElement('div');
+    final.classList.add(username);
+    const userElement = document.createElement('input');
+    const text = document.createElement('label');
+    userElement.type = "checkbox";
+    userElement.classList.add('user');
+    userElement.id = username;
+    text.innerText = username;
+    final.append(userElement);
+    final.append(text);
+    // var e=document.createElement('br');
+    // final.append(e);
+    room_container.append(final);
+}
+// if the form get submitted send message to servr
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = msgInput.value;
+    const image = imageinput.files[0];
+    if (image != null) {
+        var imageUrl = URL.createObjectURL(image);
+    }
+    message_send = [message, imageUrl];
+    append(`you:${message}`, 'right');
+    
+    if (image != null) {
+        append_img(URL.createObjectURL(image), 'right');
+    }
+   
+    var room = []
+    var all = document.getElementById("all");
+    if (all.checked == true) {
+        socket.emit('send', message_send, room);
+    } else {
+        var user = document.querySelectorAll(".user");
+        
+        for (let i = 0; i < user.length; i++) {
+            if (user[i].checked == true) {
+                room.push(user[i].id);
+            }
+        }
+        socket.emit('send', message_send, room);
+    }
+    room = [];
+    msgInput.value='';
+    
+});
+/// ask the new user his and her name and let the server know
+var username = prompt("Enter your name:");
+var you = document.getElementById("you");
+let x='User Name:';
+x+=username;
+document.getElementById("log").innerHTML = x;
+
+socket.emit('new_user_joined', username);
+socket.on("get_user_list", users_names => {
+    userlist = users_names;
+    
+    for (let i = 0; i < users_names.length; i++) {
+        append_user(users_names[i]);
+    }
+});
+// if the new user joins,recieve his/her name  from server
+socket.on('user_joined', data => {
+    append(`${data} joined the chat`, 'mid');
+    append_user(data);
+    wrapper.scrollTop = wrapper.scrollHeight;
+})
+// if the server sends a message and recieve
+socket.on('receive', data => {
+    append(`${data.name}:${data.message[0]}`, 'left');
+    if (data.message[1] != null) {
+        append_img(data.message[1], 'left');
+    }
+     // Scroll down
+     wrapper.scrollTop = wrapper.scrollHeight;
+})
+// if a user leaves the chat
+socket.on('user_disconnected', (username) => {
+    delete_element(username);
+    append(`${username} left the chat`, 'left');
+})
---
+++ server.js
@@ -0,0 +1,55 @@
+//node server which will handle socket io connection
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio=require('socket.io')
+
+var users={};
+var antiusers={};
+var users_names=[];
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+// To get files from static folder
+app.use(express.static(path.join(__dirname + 'public')));
+
+// Run when client connects
+io.on("connection",socket=>{
+    socket.on("new_user_joined",username =>{
+        users[socket.id]=username;
+        antiusers[username]=socket.id;
+        io.sockets.to(socket.id).emit("get_user_list",users_names);
+        if(username!=null){
+            users_names.push(username);
+        }
+        socket.broadcast.emit("user_joined",username);
+    });
+    socket.on('send',(message,room)=>{
+        if(room.length==0){
+            socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+        }
+        else{
+            for(let i=0;i<room.length;i++){
+                io.sockets.to(antiusers[room[i]]).emit('receive',{message:message,name:users[socket.id]});
+            }
+        }
+    });
+    socket.on("disconnect",()=>{
+        var temp=users[socket.id];
+        delete users[socket.id];
+        socket.broadcast.emit('user_disconnected',temp);
+        delete antiusers[temp];
+        var t=[];
+        for(var i=0;i<users_names.length;i++){
+            if(users_names[i]!=temp){
+                t.push(users_names[i]);
+            }
+        }
+        users_names=t;
+    });
+}); 
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
